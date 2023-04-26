624440a386c46f22d4bc4ef7491a3fc1d312cfd4
Wangleng Tan <wangleng.tan@shopee.com>
2023-04-26 14:31:30 +0800

    Add minimal working example

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,29 @@
+import React from "react";
+import { Switch } from "antd";
+import { Form } from "@rjsf/antd";
+import { RJSFSchema } from "@rjsf/utils";
+import validator from "@rjsf/validator-ajv8";
+
+const SCHEMA: RJSFSchema = {
+  title: "My Form",
+  description: "This is my form",
+  type: "object",
+};
+
 export default function App() {
-  return <div>TODO</div>
+  const [addIdPrefix, setAddIdPrefix] = React.useState(false);
+
+  return (
+    <>
+      <div style={{ marginBottom: 8 }}>
+        <Switch onChange={(checked) => setAddIdPrefix(checked)} /> Add idPrefix
+        to Form
+      </div>
+      <Form
+        idPrefix={addIdPrefix ? "randomPrefix" : undefined}
+        schema={SCHEMA}
+        validator={validator}
+      />
+    </>
+  );
 }
